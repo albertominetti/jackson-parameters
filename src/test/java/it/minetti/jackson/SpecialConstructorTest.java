10ef819afcfa9171dec2194f0bd5d1d7fafa8d03
Alberto Minetti <alberto.minetti@gmail.com>
2020-05-04 15:45:56 +0200

    additional test using annotations

--- src/test/java/it/minetti/jackson/SpecialConstructorTest.java
+++ src/test/java/it/minetti/jackson/SpecialConstructorTest.java
@@ -1,5 +1,7 @@
 package it.minetti.jackson;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -62,5 +64,42 @@ public class SpecialConstructorTest {
         // Lombok generates a constructor with parameters for field1 and field2
         // Lombok generates the setter for the field3
     }
+
+
+    @Test
+    void deserialize_with_special_constructor_annotations_success() throws JsonProcessingException {
+        // given
+        ObjectMapper mapper = JsonMapper.builder()
+                .build();
+
+        MyClassWithSpecialConstructorWithAnnotations original = new MyClassWithSpecialConstructorWithAnnotations("one", "two");
+        original.setField3("three");
+        String serialized = mapper.writeValueAsString(original);
+
+        // when
+        MyClassWithSpecialConstructorWithAnnotations deserialized = mapper.readValue(serialized, MyClassWithSpecialConstructorWithAnnotations.class);
+
+        // then
+        assertThat(deserialized, is(notNullValue()));
+        assertThat(deserialized.getField1(), is("one"));
+        assertThat(deserialized.getField2(), is("two"));
+        assertThat(deserialized.getField3(), is("three"));
+    }
+
+
+    @Data
+    private static class MyClassWithSpecialConstructorWithAnnotations {
+        private final String field1;
+        private final String field2;
+        private String field3;
+        // Lombok generates the setter for the field3
+
+        @JsonCreator
+        public MyClassWithSpecialConstructorWithAnnotations(@JsonProperty("field1") String field1, @JsonProperty("field2") String field2) {
+            this.field1 = field1;
+            this.field2 = field2;
+        }
+    }
+
 
 }
